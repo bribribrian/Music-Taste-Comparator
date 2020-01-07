b8de28331a7d7acd4d0851f338ea863b10cb1962
Brian Klein <Bribribrian@gmail.com>
2020-01-06 20:56:45 -0500

    Builds up routing

---
+++ bin/server.js
@@ -0,0 +1,18 @@
+const app = require('../app');
+const http = require('http');
+const debug = require('debug')
+
+const port = 8080
+
+app.set('port', port);
+let server = http.createServer(app);
+server.listen(port);
+server.on('listening', onListening);
+
+function onListening() {
+    let addr = server.address();
+    let bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
-// import "./styles/index.scss";
-// const testObj = {
-//   key1: "hi",
-//   key2: {
-//     key3: "Hello"
-//   }
-// };
 
-// const greeting = testObj?.key2?.key3 || testObj.key1;
-// window.addEventListener("DOMContentLoaded", () => {
-//   document.body.classList.add("center");
-//   const card = document.createElement("div");
-//   card.classList.add("card", "center");
-//   card.innerHTML = `<h2>${greeting} World!</h2>`;
-//   document.body.append(card);
-// });
 const express = require('express');
 const router = express.Router();
-const spotifyAPI = require('../API/spotify_api.js');
+const spotifyAPI = require('../API/spotify_api');
 
 router.get('/callback', function (req,res) {
   spotifyAPI.spotifyAuth(req, res)
+});
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+      title: 'Music Taste Comparator'
+  });
 });
 
 router.get('/login', function (req, res) {
@@ -27,4 +18,9 @@ router.get('/login', function (req, res) {
   spotifyAPI.spotifyLogin(req, res)
 });
 
-mnodule.exports = router;
+router.get('/app', function (req, res) {
+  console.log(req.session);
+  console.log('HIIIIII');
+});
+
+module.exports = router;
