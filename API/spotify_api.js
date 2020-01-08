ab07b5db20f04a545b03e3d447a616ce3a5ae713
Brian Klein <Bribribrian@gmail.com>
2020-01-07 21:53:12 -0500

    Populates user track list from API

--- API/spotify_api.js
+++ API/spotify_api.js
@@ -15,12 +15,13 @@ function randomString(length) {
     let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let charsLength = chars.length;
     for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charsLength));
+        result += chars.charAt(Math.floor(Math.random() * charsLength));
     }
     return result;
 }
 
 module.exports.spotifyLogin = function (res) {
+    // console.log(res)
     let state = randomString(16);
     res.cookie(stateKey, state);
     let scope = 'user-top-read';
@@ -36,11 +37,55 @@ module.exports.spotifyLogin = function (res) {
 }
 
 module.exports.spotifyAuth = function (req, res) {
+    let track_info = [];
+    let track_attributes = [];
     spotifyAPI.authorizationCodeGrant(req.query.code).then(function (data) {
     spotifyAPI.setAccessToken(data.body.access_token);
     spotifyAPI.setRefreshToken(data.body.refresh_token);
-    return spotifyAPI.getMe();
-    }).then(
-        console.log(userData)
-    ).catch(error => console.log(error));
-};
+    return spotifyAPI.getMe()
+    }).then(function () {
+        spotifyAPI
+        .getMyTopTracks({ limit: 5 })
+        .then(function (data) {
+            // console.log(data.body.items);
+            return data.body.items.map(track => {
+            // data.body.items.forEach(track => {
+                track_info.push({ [track.id]: [track.name, track.artists[0].name]})
+                console.log(track_info);
+                // console.log(data.body.items);
+                return track.id
+            })
+        })
+        .then(function (trackIds) {
+            return spotifyAPI.getAudioFeaturesForTracks(trackIds)
+        })
+        .then(function (data) {
+            // console.log(data)
+            // console.log(data.body.audio_features)
+            data.body.audio_features.forEach((track, idx) => {
+                song_name = track_info[idx][track.id][0]
+                artist_name = track_info[idx][track.id][1]
+                    track_attributes.push({
+                    "title": song_name,
+                    "artist": artist_name,
+                    "idx": idx,
+                    "energy": track.energy,
+                    "track_id": track.id
+                })
+            // console.log(track_attributes)
+            })
+
+    // }).then( function (data) {
+        req.session.track_info = track_info
+        req.session.track_attributes = track_attributes
+        // req.session.session_data = data
+        // console.log(req.session)
+        // console.log(data)
+        res.redirect('/app')
+        })
+    }).catch(error => console.log(error));
+}
+
+module.exports.getSearchedUser = function (req, res) {
+    spotifyAPI
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 const spotifyAPI = require('../API/spotify_api');
 
+
+router.get('/searcheduser', function (req, res) {
+  spotifyAPI.getSearchedUser(req, res)
+});
+
 router.get('/callback', function (req,res) {
   spotifyAPI.spotifyAuth(req, res)
 });
@@ -15,12 +20,20 @@ router.get('/', function (req, res, next) {
 
 router.get('/login', function (req, res) {
   debugger;
-  spotifyAPI.spotifyLogin(req, res)
+  spotifyAPI.spotifyLogin(res)
 });
 
 router.get('/app', function (req, res) {
-  console.log(req.session);
-  console.log('HIIIIII');
+  const { track_attributes } = req.session
+  const { track_info } = req.session
+  // const session_data = req.session
+  res.render('visualization', {
+    // session_data: session_data,
+    track_info: track_info,
+    track_attributes: track_attributes
+  })
+  // console.log('HIIIIII')
+  // console.log(req.session);
 });
 
 module.exports = router;
